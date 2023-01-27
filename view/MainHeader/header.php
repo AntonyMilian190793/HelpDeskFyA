3c82160534997ea7a31045b3beb556b1cbd64798
AntonyMilian190793 <antonymilian19@gmail.com>
2023-01-27 12:42:14 -0500

    27-01-2023

--- view/MainHeader/header.php
+++ view/MainHeader/header.php
@@ -1,7 +1,7 @@
 <header class="site-header">
     <div class="container-fluid">
 
-        <a href="#" class="site-logo">
+        <a href="..\Home\" class="site-logo">
             <img class="hidden-md-down" src="../../public/img/logo.png" alt="">
             <img class="hidden-lg-up" src="../../public/img/logo.png" alt="">
         </a>
